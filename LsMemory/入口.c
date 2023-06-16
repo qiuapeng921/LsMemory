e4b68f06d8317613b09994194468f9bee5e70fad
qiuapeng <qiuapeng@vchangyi.com>
2023-06-16 15:00:06 +0800

    update

--- LsMemory/入口.c
+++ LsMemory/入口.c
@@ -4,9 +4,39 @@
 #include "读写.h"
 
 
+NTSTATUS InitDeviceSymbolic(PDRIVER_OBJECT Driver) {
+	NTSTATUS status = STATUS_SUCCESS;
+	PDEVICE_OBJECT pdeojb = { 0 };
+
+	status = IoCreateDevice(Driver, 0, &usDeivceName, FILE_DEVICE_UNKNOWN, 0, 0, &pdeojb);
+
+	if (!NT_SUCCESS(status)) {
+
+		KdPrint(("[LsMemory]:failed to create device\n"));
+
+		return status;
+
+	}
+
+	status = IoCreateSymbolicLink(&usSymbolicName, &usDeivceName);
+
+	if (!NT_SUCCESS(status)) {
+
+		IoDeleteDevice(pdeojb);
+
+		KdPrint(("[LsMemory]:failed to create symbolic name\n"));
+
+		return status;
+	}
+
+	KdPrint(("[LsMemory]:Create Link Success\n"));
+
+	return status;
+
+}
+
 NTSTATUS DispatchCreateClose(PDEVICE_OBJECT pDevObj, PIRP pIrp)
 {
-	KdPrint(("调度创建关闭!\n"));
 	UNREFERENCED_PARAMETER(pDevObj);
 	UNREFERENCED_PARAMETER(pIrp);
 
@@ -19,11 +49,13 @@ NTSTATUS DispatchCreateClose(PDEVICE_OBJECT pDevObj, PIRP pIrp)
 NTSTATUS DispatchIoctl(PDEVICE_OBJECT pDevObj, PIRP pIrp)
 {
 	UNREFERENCED_PARAMETER(pDevObj);
+	NTSTATUS status = STATUS_SUCCESS;
 	ULONG i = 0, code = 0, len = 0, size = 0;
-	PIO_STACK_LOCATION stack = NULL;
 
-	stack = IoGetCurrentIrpStackLocation(pIrp);
+	PIO_STACK_LOCATION stack = IoGetCurrentIrpStackLocation(pIrp);
 	code = stack->Parameters.DeviceIoControl.IoControlCode;
+
+	KdPrint(("[LsMemory]:failed to create device\n"));
 
 	switch (code) {
 	case IOCTL_GET_SECTION_ADDRESS:
@@ -62,9 +94,8 @@ void DriverUnload(PDRIVER_OBJECT pDrvObj)
 {
 	UNICODE_STRING usSymboName;
 
-	KdPrint(("驱动卸载!\n"));
-	RtlInitUnicodeString(&usSymboName, DEVICE_NAME);
-	IoDeleteSymbolicLink(&usSymboName);
+	KdPrint(("[LsMemory]:Unload Successly\r\n"));
+	IoDeleteSymbolicLink(&usSymbolicName);
 	if (pDrvObj->DeviceObject != NULL) {
 		IoDeleteDevice(pDrvObj->DeviceObject);
 	}
@@ -72,20 +103,22 @@ void DriverUnload(PDRIVER_OBJECT pDrvObj)
 
 NTSTATUS DriverEntry(PDRIVER_OBJECT pDrvObj, PUNICODE_STRING pRegPath)
 {
-	PDEVICE_OBJECT pFunObj = NULL;
-	UNICODE_STRING usDeviceName;
-	UNICODE_STRING usSymboName;
+	NTSTATUS status = STATUS_SUCCESS;
+
 	UNREFERENCED_PARAMETER(pRegPath);
 
-	KdPrint(("驱动加载!\n"));
-	RtlInitUnicodeString(&usDeviceName, DRIVER_NAME);
-	IoCreateDevice(pDrvObj, 0, &usDeviceName, FILE_DEVICE_UNKNOWN, 0, FALSE, &pFunObj);
-	RtlInitUnicodeString(&usSymboName, DEVICE_NAME);
-	IoCreateSymbolicLink(&usSymboName, &usDeviceName);
+	KdPrint(("[LsMemory]:Driver Load!\n"));
 
-	// pDrvObj->MajorFunction[IRP_MJ_CREATE] =
+	pDrvObj->DriverUnload = DriverUnload;
+
+	status = InitDeviceSymbolic(pDrvObj);
+	if (!NT_SUCCESS(status)) {
+		return status;
+	}
+
+	pDrvObj->MajorFunction[IRP_MJ_CREATE] =
 	pDrvObj->MajorFunction[IRP_MJ_CLOSE] = DispatchCreateClose;
 	pDrvObj->MajorFunction[IRP_MJ_DEVICE_CONTROL] = DispatchIoctl;
-	pDrvObj->DriverUnload = DriverUnload;
-	return STATUS_SUCCESS;
+	
+	return status;
 }
--- LsMemory/标头.h
+++ LsMemory/标头.h
@@ -1,5 +1,5 @@
-#define DRIVER_NAME                       L"\\Device\\helper"
-#define DEVICE_NAME                       L"\\??\\helper"
+UNICODE_STRING usDeivceName = RTL_CONSTANT_STRING(L"\\Device\\LsMemory");
+UNICODE_STRING usSymbolicName = RTL_CONSTANT_STRING(L"\\??\\LsMemory");
 
 #define IOCTL_GET_SECTION_ADDRESS	    CTL_CODE(FILE_DEVICE_UNKNOWN, 801, METHOD_BUFFERED, FILE_ANY_ACCESS)
 #define IOCTL_READ_MEMORY               CTL_CODE(FILE_DEVICE_UNKNOWN, 802, METHOD_BUFFERED, FILE_ANY_ACCESS)
